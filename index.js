94eef38754afaf8bddeb2d9e028006edb42b99d1
Parker Fisher <pfishe13@kent.edu>
2022-07-15 14:42:14 -0400

    Change to Class structure

--- index.js
+++ index.js
@@ -1,88 +1,109 @@
-let myLibrary = [];
-let totalBooks = 0;
+class LibraryClass {
+    totalBooks = 0;
+    myLibrary = [];
 
-function Book(title, author, numPages, genre, haveRead) {
-    this.id = totalBooks;
-    this.title = title;
-    this.author = author;
-    this.numPages = numPages;
-    this.genre = genre;
-    this.haveRead = (Boolean(haveRead));
-}
-
-let Book1 = new Book("Harry Potter", "J.K Rowling", 345, "Fantasy", true);
-addBookToLibrary(Book1);
-
-function addBookToLibrary(newBook) {
-    totalBooks++;
-    myLibrary.push(newBook);
-    loadBooks();
-}
-
-function deleteBook(e) {
-    const bookID = e.target.id;
-    const book = myLibrary.find(book => book.id == bookID);
-    let bookIndex = myLibrary.indexOf(book);
-    myLibrary.splice(bookIndex, 1);
-    loadBooks();
-}
-
-function loadBooks() {
-    let books = document.getElementById("book-container");
-    // Remove all child elements 
-    var firstChild = books.firstElementChild;
-    while(firstChild) {
-        firstChild.remove();
-        firstChild = books.firstElementChild;
+    constructor() {
+        this.myLibrary = [];
+        this.totalBooks = 0;
     }
 
-    myLibrary.forEach(function(Book){
-        let newBookCard = document.createElement('div');
-        newBookCard.classList.add('book-card');
+    get bookCount() {
+        return this.totalBooks;
+    }
 
-        newBookCard.innerHTML = `<h2>${Book.title}</h2>
-        <div class="book-info">
-            <h3>Author: ${Book.author}</h3>
-            <h3>Pages: ${Book.numPages}</h3>
-            <h3>Genre: ${Book.genre}</h3>
-        </div>
-        <div class="book-buttons">
-            <div class="switch-container">
-            <label class="switch">
-                <input type="checkbox" id="switch">
-                <span class="slider round"></span>
-            </label>
-            <h3 class="switch-label">${ (Book.haveRead) ? `Completed` : `Incomplete` }</h3>
-            </div>
-            <button class="delete-button" id=${Book.id}><span class="material-symbols-outlined">
-            delete
-            </span></button>
-        </div>`;
+    incrementBookCount = () => {
+        this.totalBooks++;
+    }
 
-        // Add event listener to delete button
-        const buttonElement = newBookCard.querySelector("button");
-        buttonElement.addEventListener('click', deleteBook);
+    addBookToLibrary = (newBook) => {
+        this.incrementBookCount();
+        this.myLibrary.push(newBook);
+        this.loadBooks();
+    }
 
-        const checkbox = newBookCard.querySelector("#switch");
-        if (Book.haveRead) {
-            checkbox.checked = true;
-        }
-
-        const switchLabel = newBookCard.querySelector(".switch-label");
-        // Add event listener to read sitch
-        checkbox.addEventListener('change', e => {
-            if(e.target.checked) {
-                Book.haveRead = true;
-                switchLabel.textContent = `Completed`;
-            } else {
-                Book.haveRead = false;
-                switchLabel.textContent = `Incomplete`;
+    loadBooks = () => {
+            let books = document.getElementById("book-container");
+            // Remove all child elements 
+            var firstChild = books.firstElementChild;
+            while(firstChild) {
+                firstChild.remove();
+                firstChild = books.firstElementChild;
             }
-        })
+        
+            this.myLibrary.forEach(function(Book){
+                let newBookCard = document.createElement('div');
+                newBookCard.classList.add('book-card');
+        
+                newBookCard.innerHTML = `<h2>${Book.title}</h2>
+                <div class="book-info">
+                    <h3>Author: ${Book.author}</h3>
+                    <h3>Pages: ${Book.numPages}</h3>
+                    <h3>Genre: ${Book.genre}</h3>
+                </div>
+                <div class="book-buttons">
+                    <div class="switch-container">
+                    <label class="switch">
+                        <input type="checkbox" id="switch">
+                        <span class="slider round"></span>
+                    </label>
+                    <h3 class="switch-label">${ (Book.haveRead) ? `Completed` : `Incomplete` }</h3>
+                    </div>
+                    <button class="delete-button" id=${Book.id}><span class="material-symbols-outlined">
+                    delete
+                    </span></button>
+                </div>`;
+        
+                // Add event listener to delete button
+                const buttonElement = newBookCard.querySelector("button");
+                // buttonElement.addEventListener('click', deleteBook);
+                buttonElement.addEventListener('click', (e) => {
+                    const bookID = e.path[1].id;
 
-        books.appendChild(newBookCard);
-    });
+                    // console.log(this.myLibrary);
+                    const book = Library.myLibrary.find(book => book.id == bookID);
+                    let bookIndex = Library.myLibrary.indexOf(book);
+                    Library.myLibrary.splice(bookIndex, 1);
+                    Library.loadBooks();
+                } )
+        
+                const checkbox = newBookCard.querySelector("#switch");
+                if (Book.haveRead) {
+                    checkbox.checked = true;
+                }
+        
+                const switchLabel = newBookCard.querySelector(".switch-label");
+                // Add event listener to read sitch
+                checkbox.addEventListener('change', e => {
+                    if(e.target.checked) {
+                        Book.haveRead = true;
+                        switchLabel.textContent = `Completed`;
+                    } else {
+                        Book.haveRead = false;
+                        switchLabel.textContent = `Incomplete`;
+                    }
+                })
+        
+                books.appendChild(newBookCard);
+            });
+        }
 }
+
+class Book {
+    constructor(title, author, numPages, genre, haveRead) {
+        this.title = title;
+        this.author = author;
+        this.numPages = numPages;
+        this.genre = genre;
+        this.haveRead = (Boolean(haveRead));
+        this.id = Library.totalBooks;
+    }
+}
+
+const Library = new LibraryClass();
+let Book1 = new Book("Harry Potter", "J.K Rowling", 345, "Fantasy", true, Library.bookCount);
+Library.addBookToLibrary(Book1);
+
+
 
 // Form
 function openForm() {
@@ -108,8 +129,10 @@ form.addEventListener('submit', function(e) {
     let pages = document.getElementById('pages').value
     let genre = document.getElementById('genre').value
     let hasRead = document.getElementById('hasRead').checked;
-    let newBook = new Book(title, author, pages, genre, hasRead);
-    addBookToLibrary(newBook);
+    let newBook = new Book(title, author, pages, genre, hasRead, Library.bookCount);
+
+    Library.addBookToLibrary(newBook);
+
     closeForm();
     form.reset();
 });
